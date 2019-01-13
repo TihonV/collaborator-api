e2998a2eb3d86660b05590e27b7ef8201a346431
tihon@ciat.net.ua <tihon@gmail.com>
2019-01-14 00:43:05 +0200

    Collect static

--- chem_collaborator/settings.py
+++ chem_collaborator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3ov(h1*j$a_b27e+0u46(u(!8s1-nu2pnch$(2+got8ks2wic3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
